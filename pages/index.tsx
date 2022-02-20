41746be6002d0939d574bf4e0c535f7f098802ac
Martin-P95 <75830632+Martin-P95@users.noreply.github.com>
2022-02-20 09:06:09 +0100

    spokojenost

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-   <>
-   <h1>ahoj světe</h1>
-   </>
-  )
-}
+    <>
+      <div className="hlavnís">
+        <div className="navbar">
+          <img src="/LogoR.svg" alt="" className="logo" />
+        </div>
+        <div className="container">
+          <h1 className="textn">7</h1>
+          <div className="inputy">
+            <h3 className="text2">Zadejte číslo:</h3>
+            <input type="number" className="input" />
+            <h3 className="text2">Zadejte číslo:</h3>
+            <input type="number" className="input" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
