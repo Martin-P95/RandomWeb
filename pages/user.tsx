02dadf0141e75a9cb0e2192f361b4ab2d1f2d3bc
Martin-P95 <75830632+Martin-P95@users.noreply.github.com>
2022-06-08 11:01:17 +0200

    oprava listu

--- pages/user.tsx
+++ pages/user.tsx
@@ -3,14 +3,21 @@ import { Number, PrismaClient } from "@prisma/client";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import axios from "../lib/axios.client";
+import jwt from "jsonwebtoken";
+
+type TokenData = {
+  id: number;
+};
 
 type Props = {
   numbers: Array<Number>;
 };
 
-export const getServerSideProps: GetServerSideProps = async (_context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = jwt.verify(context.req.cookies.token, process.env.JWT_SECRET!) as TokenData;
+  const userId = data.id;
   const prisma = new PrismaClient();
-  const numbers = await prisma.number.findMany();
+  const numbers = await prisma.number.findMany({ where: { userId } });
   return {
     props: {
       numbers,
@@ -35,7 +42,6 @@ export default function User({ numbers }: Props) {
             <button onClick={() => remove(number.id)} className="delete">
               <img src="trash-2.svg" alt="" />
             </button>
-            
           </div>
         ))}
       </section>
